f41fc92e616137d84f325f8e6c5111f61bdd7c50
Vladislav Veschev <vladeiko999@gmail.com>
2021-11-05 22:53:14 +0300

    Update packages to latest versions, prepare project to start

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    'react': {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "always",
+  printWidth: 120,
+  quoteProps: "consistent",
+  endOfLine: "auto",
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  singleQuote: true,
+  jsxBracketSameLine: true,
+};
