d4884a90c8faf73f9f117f26c33da0b63ddb80ff
Vladislav Veschev <vladeiko999@gmail.com>
2021-11-08 01:14:57 +0300

    Add ComputersList page layout

--- src/pages/ComputersList/index.jsx
+++ src/pages/ComputersList/index.jsx
@@ -1,5 +1,22 @@
+import { Button, Table } from 'antd';
+
+import './style.scss';
+
 const ComputersListPage = () => {
-  return <div>Computers List</div>;
+  return (
+    <div className="computers-list">
+      <div className="computers-list__title">
+        <span className="title">Список компьютеров</span>
+        <span className="description">
+          Какой-то ещё не придуманный текст, который будет отражать основную часть этой части приложения
+        </span>
+        <Button type="primary" className="add-button">Добавить</Button>
+      </div>
+      <div className="computers-list__table">
+        <Table />
+      </div>
+    </div>
+  );
 };
 
 export default ComputersListPage;
