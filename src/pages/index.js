abc593726aee8a6384d6ec1548a4c50e4c49fdc9
Vladislav Veschev <vladeiko999@gmail.com>
2021-11-22 21:36:37 +0300

    Fix soft remove

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,4 @@ export { default as ComputersList } from './ComputersList';
 export { default as Computer } from './Computer';
 export { default as SignInPage } from './SignIn';
 export { default as SoftList } from './SoftList';
-export { default as TasksList } from './TasksList';
 export { default as UsersList } from './UsersList';
