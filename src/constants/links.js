4ee461f646aaaf7b1ec128abe245b69c853e95d3
Vladislav Veschev <vladeiko999@gmail.com>
2021-11-22 21:35:51 +0300

    Remove tasks

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,14 +1,13 @@
 import accessLevels from './accessLevels';
 
-import { HomeOutlined, CheckOutlined, UsergroupAddOutlined, DesktopOutlined } from '@ant-design/icons';
+import { HomeOutlined, UsergroupAddOutlined, DesktopOutlined } from '@ant-design/icons';
 
 const home = { name: 'На Главную', path: 'home', Icon: HomeOutlined };
 const computers = { name: 'Компьютеры', path: 'computers', Icon: DesktopOutlined };
-const tasks = { name: 'Задачи', path: 'tasks', Icon: CheckOutlined };
 const users = { name: 'Пользователи', path: 'users', Icon: UsergroupAddOutlined };
 
 export default {
-  [accessLevels.SYS_ADMIN]: [home, tasks],
-  [accessLevels.MANAGER]: [home, computers, users, tasks],
-  [accessLevels.ADMIN]: [home, computers, users, tasks],
+  [accessLevels.SYS_ADMIN]: [home],
+  [accessLevels.MANAGER]: [home, computers, users],
+  [accessLevels.ADMIN]: [home, computers, users],
 };
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,23 +1,20 @@
 import accessLevels from './accessLevels';
 
-import { SoftList, TasksList, UsersList } from '../pages';
+import { SoftList, UsersList } from '../pages';
 import { ComputersModule } from '../modules';
 
 export default {
   [accessLevels.SYS_ADMIN]: [
     { path: 'home', component: ComputersModule },
-    { path: 'tasks', component: TasksList },
   ],
   [accessLevels.MANAGER]: [
     { path: 'home', component: SoftList },
     { path: 'computers/*', component: ComputersModule },
-    { path: 'tasks', component: TasksList },
     { path: 'users', component: UsersList },
   ],
   [accessLevels.ADMIN]: [
     { path: 'home', component: UsersList },
     { path: 'computers', component: ComputersModule },
-    { path: 'tasks', component: TasksList },
     { path: 'users', component: UsersList },
   ],
 };
